6c584ee08933eab3328e76deb336ad2adb91d7b7
Jjuhui313 <hyacinth92928@gmail.com>
2025-11-17 21:12:27 +0900

    [#1]Feat: User Entity, Signup/LoginRequest, TokenResponse, CustomUser, UserRepository 생성 후 SecurityConfig 수정

---
+++ src/main/java/com/twohundredone/taskonserver/auth/dto/EmailFormatValidator.java
@@ -0,0 +1,20 @@
+package com.twohundredone.taskonserver.auth.dto;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EmailFormatValidator implements ConstraintValidator<CustomEmail, String> {
+    @Override
+    public void initialize(CustomEmail constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        return value.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    }
+}
---
+++ src/main/java/com/twohundredone/taskonserver/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.twohundredone.taskonserver.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true, length = 254)
+    private String email;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String profileImageUrl;
+
+    // 추후 OAuth2용
+    @Column(length = 50)
+    private String provider; // "kakao", "google" 등
+
+    @Column(length = 100)
+    private String providerUserId;
+}
