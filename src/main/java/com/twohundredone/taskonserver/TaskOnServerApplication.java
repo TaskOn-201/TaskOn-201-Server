f178163072a8319e6ed9a35d4eb0ac57d4071061
Jjuhui313 <hyacinth92928@gmail.com>
2025-11-17 11:47:27 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/com/twohundredone/taskonserver/TaskOnServerApplication.java
@@ -0,0 +1,13 @@
+package com.twohundredone.taskonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOnServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskOnServerApplication.class, args);
+    }
+
+}
