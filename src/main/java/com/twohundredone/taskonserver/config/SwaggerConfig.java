7f61a1af79d136bbd510892ea46f9bf8d41080b6
Jjuhui313 <hyacinth92928@gmail.com>
2025-12-03 22:48:50 +0900

    [#28]Fix: Security에 vercel 도메인 수정, SwaggerConfig에 서버 주소 자동 감지 설정 추가

--- src/main/java/com/twohundredone/taskonserver/config/SwaggerConfig.java
+++ src/main/java/com/twohundredone/taskonserver/config/SwaggerConfig.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -28,4 +32,13 @@ import org.springframework.context.annotation.Configuration;
         paramName = "refreshToken"
         )
 public class SwaggerConfig {
+
+        // ⭐ Swagger 서버 주소 자동 감지 설정
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        .components(new Components())
+                        // swagger servers 를 "/" 로 설정 → https 도메인 자동 인식됨
+                        .addServersItem(new Server().url("/"));
+        }
 }
