10bd3dd1c33a83eba47481f405b11efa1c216eee
Jjuhui313 <hyacinth92928@gmail.com>
2025-12-05 19:59:56 +0900

    [#1]Feat: 내 정보 조회 api 개발

--- src/main/java/com/twohundredone/taskonserver/user/service/UserService.java
+++ src/main/java/com/twohundredone/taskonserver/user/service/UserService.java
@@ -7,13 +7,18 @@ import static com.twohundredone.taskonserver.global.enums.ResponseStatusError.US
 import com.twohundredone.taskonserver.global.exception.CustomException;
 import com.twohundredone.taskonserver.global.s3.S3Uploader;
 import com.twohundredone.taskonserver.global.util.FileValidator;
+import com.twohundredone.taskonserver.project.entity.ProjectMember;
+import com.twohundredone.taskonserver.project.repository.ProjectMemberRepository;
+import com.twohundredone.taskonserver.user.dto.UserMeResponse;
 import com.twohundredone.taskonserver.user.dto.UserPasswordUpdateRequest;
 import com.twohundredone.taskonserver.user.dto.UserProfileResponse;
 import com.twohundredone.taskonserver.user.dto.UserProfileUpdateRequest;
 import com.twohundredone.taskonserver.user.entity.User;
 import com.twohundredone.taskonserver.user.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,14 +32,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final S3Uploader s3Uploader;
     private final PasswordEncoder passwordEncoder;
+    private final ProjectMemberRepository projectMemberRepository;
 
     @Transactional
     public UserProfileResponse updateProfile(Long userId, UserProfileUpdateRequest request, MultipartFile profileImage) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
-
-        log.info(">>> request.name = {}", request.getName());
-
 
         if (request.getName() != null && !request.getName().isBlank()) {
             user.updateName(request.getName());
@@ -56,6 +59,32 @@ public class UserService {
                 .profileImageUrl(user.getProfileImageUrl())
                 .build();
     }
+
+    @Transactional(readOnly = true)
+    public UserMeResponse getMyInfo(Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        List<ProjectMember> memberships =
+                projectMemberRepository.findAllByUser_UserId(userId);
+
+        List<UserMeResponse.ProjectInfo> joinedProjects = memberships.stream()
+                .map(pm -> UserMeResponse.ProjectInfo.builder()
+                        .projectId(pm.getProject().getProjectId())
+                        .projectName(pm.getProject().getProjectName())
+                        .build()
+                ).toList();
+
+        return UserMeResponse.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .profileImageUrl(user.getProfileImageUrl())
+                .joinedProjects(joinedProjects)
+                .build();
+    }
+
 
     @Transactional
     public void updatePassword(Long userId, UserPasswordUpdateRequest request) {
